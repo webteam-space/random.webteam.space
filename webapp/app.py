b18c9fb9bd6b92dcfb2fed003c23e5f211c31b7e
Thomas Bille <toto@canonical.com>
2020-02-07 15:07:31 +0000

    Use FlaskBase

--- webapp/app.py
+++ webapp/app.py
@@ -1,49 +1,15 @@
-import flask
-from werkzeug.contrib.fixers import ProxyFix
-from werkzeug.debug import DebuggedApplication
+from canonicalwebteam.flask_base.app import FlaskBase
 
-from webapp.handlers import add_headers, clear_trailing_slash
 from webapp.webteam.views import webteam
 
 
-def create_app(testing=False):
-    app = flask.Flask(
-        __name__, template_folder="../templates", static_folder="../static"
-    )
+app = FlaskBase(
+    __name__,
+    "random.webteam.space",
+    template_folder="../templates",
+    static_folder="../static",
+    template_404="404.html",
+    template_500="500.html",
+)
 
-    app.testing = testing
-
-    app.wsgi_app = ProxyFix(app.wsgi_app)
-    app.url_map.strict_slashes = False
-
-    if app.debug:
-        app.wsgi_app = DebuggedApplication(app.wsgi_app)
-
-    app.before_request(clear_trailing_slash)
-    app.after_request(add_headers)
-
-    init_handler(app)
-    init_blueprint(app)
-
-    return app
-
-
-def init_handler(app):
-    @app.errorhandler(404)
-    def page_not_found(error):
-        """
-        For 404 pages, display the 404.html template,
-        passing through the error description.
-        """
-
-        return flask.render_template("404.html", error=error.description), 404
-
-    @app.route("/_status/check")
-    def health_check():
-        """ Health check end point used by Talisker.
-        """
-        return ("", 200)
-
-
-def init_blueprint(app):
-    app.register_blueprint(webteam)
+app.register_blueprint(webteam)
